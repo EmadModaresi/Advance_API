936bf85382ab43e354edad7e8373971e4d396002
Emad Modaresi <emadmodaresi@gmail.com>
2021-12-15 11:22:07 +0330

    add a Book model to project

--- API/Advance/models.py
+++ API/Advance/models.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+
 from django.db import models
+class BookModel(models.Model):
+    name = models.CharField(max_length=50)
+    auth = models.CharField(max_length=50)
+    store = models.TextField(default=None)
+    created_at = models.DateTimeField(auto_now_add=datetime.now())
 
 # Create your models here.
